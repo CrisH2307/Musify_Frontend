835cb7562534fac0593456bc686d50e156260aeb
Cris Huynh <119613966+CrisH2307@users.noreply.github.com>
2024-03-01 16:56:16 -0500

    Update Bootstrap

--- Musify/settings.py
+++ Musify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Application definition
 
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'Musify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [BASE_DIR / "templates"], 
+        "DIRS": [os.path.join(BASE_DIR / "templates")], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
